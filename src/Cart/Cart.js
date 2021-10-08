3a35902dd333f94771c50d6696a28ca6aadf06b2
CJ <ccrju19@gmail.com>
2021-10-07 23:21:01 -0400

    edited cart list ui layout

--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -18,7 +18,7 @@ const useStyles = makeStyles((theme) => ({
 
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
-  const totalAmount = `$${cartCtx.totalAmount.toFixed(2).replace('-0', '0')}`;
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2).replace("-0", "0")}`;
   const classes = useStyles();
 
   const cartItemRemoveHandler = (id) => {
@@ -36,55 +36,53 @@ const Cart = (props) => {
   console.log(cartCtx.totalAmount);
 
   return (
-    <>
-      <Grid container>
-        <Grid item xs={3} />
-        <Grid item xs={6}>
-          <h1>Cart</h1>
-          {cartCtx.totalAmount <= 0 && 'You have no items in your cart'}
-          {cartCtx.items.map((product) => (
-            <CartProduct
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              image={product.img}
-              amount={product.amount}
-              onAdd={cartItemAddHandler.bind(null, product)}
-              onRemove={cartItemRemoveHandler.bind(null, product.id)}
-              onDelete={cartItemDeleteHandler.bind(null, product.id)}
-            />
-          ))}
-          <div>
-            <br></br>
-            <br></br>
-            <Divider variant="fullWidth" /> <br></br>
-            <h3>Subtotal: {totalAmount}</h3>
-            <h3>Total: {totalAmount}</h3>
-            <br></br>
-            {cartCtx.totalAmount > 0 && (
-              <Link
-                component={RouterLink}
-                to="/checkout"
-                color="inherit"
-                underline="none"
+    <Grid container>
+      <Grid item xs={1} />
+      <Grid item xs={10}>
+        <h1>Cart</h1>
+        {cartCtx.totalAmount <= 0 && "You have no items in your cart"}
+        {cartCtx.items.map((product) => (
+          <CartProduct
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            image={product.img}
+            amount={product.amount}
+            onAdd={cartItemAddHandler.bind(null, product)}
+            onRemove={cartItemRemoveHandler.bind(null, product.id)}
+            onDelete={cartItemDeleteHandler.bind(null, product.id)}
+          />
+        ))}
+        <div>
+          <br></br>
+          <br></br>
+          <Divider variant="fullWidth" /> <br></br>
+          <h3>Subtotal: {totalAmount}</h3>
+          <h3>Total: {totalAmount}</h3>
+          <br></br>
+          {cartCtx.totalAmount > 0 && (
+            <Link
+              component={RouterLink}
+              to="/checkout"
+              color="inherit"
+              underline="none"
+            >
+              <Button
+                variant="outlined"
+                size="small"
+                className={classes.button}
               >
-                <Button
-                  variant="outlined"
-                  size="small"
-                  className={classes.button}
-                >
-                  Checkout
-                </Button>
-              </Link>
-            )}
-            <br></br>
-            <br></br>
-            <br></br>
-          </div>
-        </Grid>
-        <Grid item xs={3} />
+                Checkout
+              </Button>
+            </Link>
+          )}
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
       </Grid>
-    </>
+      <Grid item xs={1} />
+    </Grid>
   );
 };
 
--- src/Cart/CartProduct.js
+++ src/Cart/CartProduct.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { makeStyles } from "@material-ui/core/styles";
+import { Grid } from "@material-ui/core";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -16,19 +17,25 @@ const useStyles = makeStyles({
     minWidth: 275,
   },
   title: {
-    fontSize: 14,
+    fontSize: 18,
   },
   pos: {
     marginBottom: 0,
   },
+  content: {
+    height: "30%",
+  },
   media: {
-    height: 0,
-    // marginLeft: 50,
-    paddingTop: "30%", // 16:9
+    height: "120%",
+    width: "100%",
+    paddingTop: "10%",
     paddingBottom: "10%",
   },
   icon: {
     fontSize: "3em",
+  },
+  description: {
+    marginTop: 10,
   },
 });
 
@@ -40,31 +47,44 @@ const CartProduct = (props) => {
 
   return (
     <Card className={classes.root}>
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          {name}
-        </Typography>
-        <Typography
-          component={"span"}
-          className={classes.pos}
-          color="textSecondary"
-        >
-          ${price} x {amount}
-          <p>Total: {updatedPrice}</p>
-        </Typography>
-      </CardContent>
-      <CardMedia className={classes.media} image={image} />
-      <CardActions>
-        <IconButton onClick={props.onDelete}>
-          <DeleteIcon />
-        </IconButton>
-        <IconButton onClick={props.onAdd}>
-          <AddIcon />
-        </IconButton>
-        <IconButton onClick={props.onRemove}>
-          <RemoveIcon />
-        </IconButton>
-      </CardActions>
+      <Grid container>
+        <Grid item xs={3}>
+          <CardContent className={classes.content}>
+            <CardMedia className={classes.media} image={image} />
+          </CardContent>
+        </Grid>
+        <Grid item xs={9} className={classes.description}>
+          <Grid container>
+              <Grid item xs={3}>
+                <Typography
+                  variant="h5"
+                  component="h2"
+                  className={classes.title}
+                >
+                  {name} x {amount}{" "}
+                </Typography>
+              </Grid>
+              <Grid item xs={9}>
+                <IconButton onClick={props.onDelete}>
+                  <DeleteIcon />
+                </IconButton>
+                <IconButton onClick={props.onAdd}>
+                  <AddIcon />
+                </IconButton>
+                <IconButton onClick={props.onRemove}>
+                  <RemoveIcon />
+                </IconButton>
+              </Grid>
+          </Grid>
+          <Typography
+            component={"span"}
+            className={classes.pos}
+            color="textSecondary"
+          >
+            <p>Total: {updatedPrice}</p>
+          </Typography>
+        </Grid>
+      </Grid>
     </Card>
   );
 };
