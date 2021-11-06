c2cdbace45e75852e94c9ef2eb6b40a7d414d4f6
CJ <ccrju19@gmail.com>
2021-11-05 20:44:07 -0400

    tested axios post call to api orders endpoint

--- src/Cart/ConfirmInfo.js
+++ src/Cart/ConfirmInfo.js
@@ -1,4 +1,5 @@
 import { React, useContext, useState } from "react";
+import axios from "axios";
 
 import Button from "@material-ui/core/Button";
 import { Grid } from "@material-ui/core";
@@ -14,6 +15,8 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const ORDERS_REST_API_URL = "http://localhost:8080/api/orders";
+
 const ConfirmInfo = (props) => {
   const classes = useStyles();
   const userContext = useContext(UserInfoContext);
@@ -22,14 +25,14 @@ const ConfirmInfo = (props) => {
   // console.log(userContext.info);
 
   const timestamp = Date.now();
-  const timeordered =  new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      }).format(timestamp)
+  const timeordered = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  }).format(timestamp);
 
   const {
     firstname,
@@ -40,20 +43,20 @@ const ConfirmInfo = (props) => {
     city,
     state,
     postal,
-    datetime
+    datetime,
   } = userContext.info;
 
   let pickupordeliverytime = "";
 
   if (datetime !== "ASAP") {
-      pickupordeliverytime = new Intl.DateTimeFormat("en-US", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      }).format(datetime)
+    pickupordeliverytime = new Intl.DateTimeFormat("en-US", {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    }).format(datetime);
   } else {
     pickupordeliverytime = "ASAP";
   }
@@ -69,29 +72,46 @@ const ConfirmInfo = (props) => {
       address: address,
       city: city,
       state: state,
-      postal: postal
+      postal: postal,
     },
     orderitems: cartCtx.items,
-    pickupordelivery: pickupordeliverytime
-  })
+    pickupordelivery: pickupordeliverytime,
+  });
+
+  let cartItems = [];
+  cartCtx.items.forEach((item) => {
+    cartItems.push({
+      quantity: `${item.amount}`,
+      product: {
+        id: item.id
+      },
+      total_price: `${(item.amount*item.price).toFixed(2)}`
+    })
+  });
+
+  console.log(cartItems);
+
+  const [testDataObject, setTestDataObject] = useState({
+    ordernumber: "Z9823",
+    dateposted: timeordered,
+    orderItems: cartItems,
+    scheduled: pickupordeliverytime,
+  });
 
   const {} = cartCtx.items;
 
   const onConfirmHandler = () => {
     props.confirmation(true);
-    console.log(userContext.info);
-    console.log(cartCtx.items);
-    console.log(cartCtx.totalAmount);
-    //   new Intl.DateTimeFormat("en-US", {
-    //     year: "numeric",
-    //     month: "2-digit",
-    //     day: "2-digit",
-    //     hour: "2-digit",
-    //     minute: "2-digit",
-    //     second: "2-digit",
-    //   }).format(timestamp)
-    // );
-    console.log(dataObject);
+    // console.log(userContext.info);
+    // console.log(cartCtx.items);
+    // console.log(cartCtx.totalAmount);
+
+    // console.log(dataObject);
+    console.log(testDataObject);
+
+    axios
+      .post(ORDERS_REST_API_URL, testDataObject)
+      .then((response) => console.log(response));
 
     cartCtx.items.forEach((item) => {
       cartCtx.deleteItem(item.id);
