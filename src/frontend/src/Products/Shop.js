87e862c869430004c1b2bb7b56f1a879d874fd83
CJ <ccrju19@gmail.com>
2022-02-25 21:18:59 -0500

    add test change to shop page

--- src/frontend/src/Products/Shop.js
+++ src/frontend/src/Products/Shop.js
@@ -95,6 +95,7 @@ const Shop = () => {
       <Grid item xs={10} sm={10}>
         <Box mt={3} mb={4}>
           <Typography variant="h4">Baked Goods</Typography>
+          <h5>Test Change</h5>
         </Box>
         <Grid container>
           <Grid item xs={12} sm={matches ? 2 : false}>
